5a125d41667917a05b5786bb827f0ef8e4607e06
Guselnikov-Maks <guselnikov.maks@gmail.com>
2025-05-18 05:44:26 +0300

    ada

--- local_settings.py
+++ local_settings.py
@@ -1,4 +1,4 @@
-mysql_user= ''                                  #user mysql
+mysql_user= 'maks'                                  #user mysql
 mysql_password= ''                              #password for user mysql
 mysql_host= ''                                  #host mysql server
 mysql_port = ''                                 #port mysql server
